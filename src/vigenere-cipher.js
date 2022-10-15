13f2076057e01ab4a6cf82557a26155234f3e53f
Konstantin Moskvin <mr.koctia@gmail.com>
2022-10-16 01:33:15 +0300

    end

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,16 +20,70 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(type) {
+    this.type = type;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
+
+  encrypt(text,alphonse) {
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    if (text === undefined || alphonse === undefined) {
+      throw new Error('Incorrect arguments!');
+    }
+    text = text.toUpperCase();
+    alphonse = alphonse.toUpperCase();
+    let result = '';
+
+    for (let i = 0, j = 0; i < text.length; i++) {
+      const charText = text[i].charCodeAt(0) - 65;
+      const charAlphonse = alphonse[j % alphonse.length].charCodeAt(0) - 65;
+      // console.log(charText, charAlphonse);
+      if (charText >= 0 && charText <= 25) {
+        result += String.fromCharCode((charText + charAlphonse) % 26 + 65);
+        // console.log(result);
+        j++;
+      } else {
+        result += text[i];
+      }
+    }
+    // console.log(this.type === false ? result.split('').reverse().join('') : result);
+    return this.type === false ? result.split('').reverse().join('') : result;
+  }
+
+  decrypt(text,alphonse) {
+    // throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
+    if (text === undefined || alphonse === undefined) {
+      throw new Error('Incorrect arguments!');
+    }
+    text = text.toUpperCase();
+    alphonse = alphonse.toUpperCase();
+    let result = '';
+
+    for (let i = 0, j = 0; i < text.length; i++) {
+      const charText = text[i].charCodeAt(0) - 65;
+      const charAlphonse = alphonse[j % alphonse.length].charCodeAt(0) - 65;
+      if (charText >= 0 && charText <= 25) {
+        result += String.fromCharCode((charText - charAlphonse + 26) % 26 + 65);
+        // console.log(result);
+        j++;
+      } else {
+        result += text[i];
+      }
+    }
+    // console.log(this.type === false ? result.split('').reverse().join('') : result);
+    return this.type === false ? result.split('').reverse().join('') : result;
   }
 }
 
 module.exports = {
   VigenereCipheringMachine
 };
+
+// const directMachine = new VigenereCipheringMachine();
+// directMachine.encrypt('attack at dawn!', 'alphonse') // => 'AEIHQX SX DLLU!'
+// directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') // => 'ATTACK AT DAWN!'
+
+// const reverseMachine = new VigenereCipheringMachine(false);
+// reverseMachine.encrypt('attack at dawn!', 'alphonse') // => '!ULLD XS XQHIEA'
+// reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') // => '!NWAD TA KCATTA'
